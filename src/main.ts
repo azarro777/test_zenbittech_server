15dc46b502f4b6988a269da3f7d77596d56dc896
@azarro777 <azarro777@gmail.com>
2022-07-25 15:54:31 +0300

    change path to deploy herocu

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen((process.env.DB_PORT, +'0.0.0.0') || 3000);
 }
 bootstrap();
